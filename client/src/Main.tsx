c1065bc1aaa189dfb864c4301b6e698d927ad734
Josh Asi <josh.asi@hotmail.co.nz>
2018-08-26 01:48:18 +1200

    Nominations Complete renders once nominate has been pressed

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Nominations from './components/NominationPage';
 import Home from './components/Home';
-import NominationComplete from "./components/NominationComplete";
 
 class Main extends React.Component {
     public render() {
@@ -11,7 +10,6 @@ class Main extends React.Component {
             <Switch>
                 <Route exact={true} path = '/' component={Home} />
                 <Route path='/nominate' component={Nominations} />
-                <Route path='/nominationscomplete' component={NominationComplete} />
             </Switch>
         </div>
       )
--- client/src/components/NominationPage.tsx
+++ client/src/components/NominationPage.tsx
@@ -4,8 +4,7 @@ import NominationJustification from "./NominationJustification";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
-
-// import {Link} from 'react-router-dom';
+import NominationComplete from "./NominationComplete";
 
 function getSteps(): string[] {
   return ["Category", "Nominee", "Justification"];
@@ -16,12 +15,13 @@ class NominationPage extends React.Component<any, any> {
     category: "Being Purple",
     nominee: "Boruto's Dad",
     justification: "He achieved his dream of becoming Hokage.",
-    activeStep: 0
+    activeStep: 0,
+    completed: false
   };
 
   public render() {
     const steps = getSteps();
-    const { activeStep } = this.state;
+    const { activeStep, completed } = this.state;
 
     const theme = createMuiTheme({
       typography: {
@@ -34,49 +34,61 @@ class NominationPage extends React.Component<any, any> {
 
     return (
       <div id="nominationPage">
-        <MuiThemeProvider theme={theme}>
-          <Stepper
-            className="stepper"
-            activeStep={activeStep}
-            alternativeLabel={true}
-          >
-            {steps.map(label => {
-              return (
-                <Step className="step" key={label}>
-                  <StepLabel className="stepLabel">{label}</StepLabel>
-                </Step>
-              );
-            })}
-          </Stepper>
-        </MuiThemeProvider>
-        <NominationJustification
-          category={this.state.category}
-          nominee={this.state.nominee}
-          justification={this.state.justification}
-        />
 
-        <div className="buttonBlock">
-          <button
-            type="button"
-            className="btn btn-primary buttonLeft"
-            onClick={this.handleBack}
-            style={
-              activeStep === 0
-                ? { visibility: "hidden" }
-                : { visibility: "visible" }
-            }
-          >
-            Back
+        {completed ?
+          <NominationComplete
+            category={this.state.category}
+            nominee={this.state.nominee}
+          />
+          
+          :
+
+          <MuiThemeProvider theme={theme}>
+            <Stepper
+              className="stepper"
+              activeStep={activeStep}
+              alternativeLabel={true}
+            >
+              {steps.map(label => {
+                return (
+                  <Step className="step" key={label}>
+                    <StepLabel className="stepLabel">{label}</StepLabel>
+                  </Step>
+                );
+              })}
+            </Stepper>
+
+            <NominationJustification
+              category={this.state.category}
+              nominee={this.state.nominee}
+              justification={this.state.justification}
+            />
+
+            <div className="buttonBlock">
+              <button
+                type="button"
+                className="btn btn-primary buttonLeft"
+                onClick={this.handleBack}
+                style={
+                  activeStep === 0
+                    ? { visibility: "hidden" }
+                    : { visibility: "visible" }
+                }
+              >
+                Back
           </button>
 
-          <button
-            type="button"
-            className="btn btn-primary buttonRight"
-            onClick={this.handleNext}
-          >
-            {activeStep === steps.length - 1 ? "Nominate" : "Next"}
-          </button>
-        </div>
+              <button
+                type="button"
+                className="btn btn-primary buttonRight"
+                onClick={this.handleNext}
+              >
+                {activeStep === steps.length - 1 ? "Nominate" : "Next"}
+              </button>
+            </div>
+
+          </MuiThemeProvider>
+        }
       </div>
     );
   }
@@ -84,8 +96,9 @@ class NominationPage extends React.Component<any, any> {
   private handleNext = () => {
     const { activeStep } = this.state;
     if (activeStep === getSteps().length - 1) {
-      // alert("I am supposed to go to the Thank You page");
-      window.location.replace("/nominationscomplete");
+      this.setState({
+        completed: true
+      });
     } else {
       this.setState({
         activeStep: activeStep + 1
