f9ad6f2e10e0d17b3056e39e4d808dd744f65f07
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2018-08-26 01:18:25 +1200

    Added peer dependencies for Bootstrap
    So that animations etc for dropdown actually work
    ohh

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,14 @@ import * as React from "react";
 import { runWithAdal } from "react-adal";
 import * as ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/jquery/dist/jquery.min.js";
+import "../node_modules/popper.js/dist/popper.min.js";
 import { authContext } from "./auth";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
-// import Card from "./components/Card";
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const DO_NOT_LOGIN = true;
 
@@ -27,12 +28,11 @@ runWithAdal(
   authContext,
   () => {
     ReactDOM.render(
-      (
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      ), 
-      document.getElementById("root") as HTMLElement);
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+      document.getElementById("root") as HTMLElement
+    );
     registerServiceWorker();
   },
   DO_NOT_LOGIN
