15cca5ae22560cb93cf102cc8e0b89364e236f07
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2018-08-26 16:30:03 +1200

    Update Home Image link

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -9,11 +9,8 @@ class Home extends React.Component<any, any> {
   public render() {
     return (
       <img
-        style={{
-          width: "100%",
-          filter: "blur(0.5px)"
-        }}
-        src="https://i.pximg.net/img-master/img/2018/03/06/21/29/43/67606282_p0_master1200.jpg"
+        style={{ width: "100%", filter: "blur(0.5px)" }}
+        src="https://pbs.twimg.com/media/DXX0Id4VMAEwa7a.jpg"
       />
     );
   }
