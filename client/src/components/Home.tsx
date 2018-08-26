1bb24aeca82f6337e1b9c6b149cc4fb43f2f22c6
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2018-08-26 16:15:48 +1200

    Overhauled Log In Screen
    Cleaned up old files

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -11,7 +11,7 @@ class Home extends React.Component<any, any> {
       <img
         style={{
           width: "100%",
-          filter: "blur(1px)"
+          filter: "blur(0.5px)"
         }}
         src="https://i.pximg.net/img-master/img/2018/03/06/21/29/43/67606282_p0_master1200.jpg"
       />
--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import "../App.css";
-import { getUser, isAuthenticated } from "../auth";
-import AppIntro from "./AppIntro";
-import LoginButton from "./LoginButton";
-import logo from "../logo.svg";
+import logo1 from "../images/logo1.png";
+import { logInUser } from "./../auth";
 
 class LoginPage extends React.Component<any, any> {
   constructor(props: any) {
@@ -12,14 +10,21 @@ class LoginPage extends React.Component<any, any> {
 
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Ignition Awards</h1>
-        </header>
-        <div className="App-user-status">
-          <AppIntro isLoggedIn={isAuthenticated()} user={getUser()} />
-          <LoginButton isLoggedIn={isAuthenticated()} />
+      <div id="loginBg">
+        <img
+          id="loginBgPic"
+          src="https://i.pinimg.com/originals/01/e7/e2/01e7e206ff9a0b29fb4bac269140dda6.jpg"
+        />
+        <div id="loginPage">
+          <img id="loginLogo" src={logo1} alt="logo" />
+          <p id="loginDesc">MYOB staff, come this way please.</p>
+          <button
+            className="btn btn-primary btn-lg"
+            id="loginButton"
+            onClick={logInUser}
+          >
+            Continue to Office 365
+          </button>
         </div>
       </div>
     );
