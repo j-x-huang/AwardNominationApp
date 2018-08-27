c0f3476cb7fc4a36541cf516664d5e9e84aea1ac
Unknown <huangxjack@gmail.com>
2018-08-27 20:37:25 +1200

    user's profile pic now shows in nav bar

--- client/src/components/NeoNavBar.tsx
+++ client/src/components/NeoNavBar.tsx
@@ -1,20 +1,38 @@
 import * as React from "react";
 import logo1 from "../images/logo1.png";
 import { getUser, logOutUser } from "../auth";
+import {getMyImage} from "../MicrosoftGraphClient"
 
 export interface INavBarProps {
   selection: number;
 }
 
 class NavBar extends React.Component<INavBarProps, any> {
+
+  public state = {
+    profilePic : 'http://www.your-pass.co.uk/wp-content/uploads/2013/09/Facebook-no-profile-picture-icon-620x389.jpg'
+  }
+
+  public componentDidMount() {
+    getMyImage((picUrl, err) => {
+      if (err) {
+        // nothing
+      } else {
+        this.setState({
+          profilePic : picUrl
+        })
+      }
+      return
+    })
+  }
+
   public render() {
     const user = getUser();
     const profileName = user.profile.name
       .split(" ")
       .slice(0, -1)
       .join(" ");
-    const profilePicUrl =
-      "https://i.pinimg.com/originals/01/e7/e2/01e7e206ff9a0b29fb4bac269140dda6.jpg";
+    // const profilePicUrl ="https://i.pinimg.com/originals/01/e7/e2/01e7e206ff9a0b29fb4bac269140dda6.jpg";
 
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -65,7 +83,7 @@ class NavBar extends React.Component<INavBarProps, any> {
                 aria-haspopup="true"
                 aria-expanded="false"
               >
-                <img id="profilePic" src={profilePicUrl} />
+                <img id="profilePic" src={this.state.profilePic} />
                 <span id="profileName">{profileName}</span>
               </a>
 
