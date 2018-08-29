3f85b020e24e9cdf8b0b3bc7743b3e890049252d
Alex Yoo <SuhoNova@gmail.com>
2018-08-30 00:38:26 +1200

    Setup express backend that verifies access_token on request

---
+++ express-backend/server.js
@@ -0,0 +1,52 @@
+var express = require('express'); 
+var app = express();
+var bodyParser = require('body-parser');
+var config = require('./config');
+var jwt = require('azure-ad-jwt');  // keys being downloaded every request TODO: cache the certificates!
+
+// SERVER CONFIGURATION
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || config.PORT;
+
+var router = express.Router();
+// all routes prefixed /api
+app.use('/api', router);
+
+// Verify Token
+router.use(function (req, res, next) {
+    var jwtToken = req.body.token || req.query.token || req.headers['access_token'];
+    if(jwtToken){
+        jwt.verify(jwtToken, null, function(err,result){
+            if(result){
+                req.auth = result;
+                next();
+            } else {
+                return res.status(401).send({ 
+                    success: false, 
+                    message: 'Failed to authenticate token.' 
+                });    
+            }
+        });
+    } else {
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token was provided.' 
+        });
+    }
+});
+
+// ROUTES FOR OUR API
+// ============================================================================================
+
+// test route (GET http://localhost:3001/api)
+router.get('/test', function(req, res) {
+    console.log("Test route");
+    console.log(req.auth);
+    res.send({ success: true, message: 'This is a test!' });   
+});
+
+// START
+app.listen(port);
+console.log("Server has started!");
