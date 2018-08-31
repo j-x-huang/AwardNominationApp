4e91e14e4a20c83af90f5d4ff5ce723de121de4b
Yuan Wei Qi <yqi955@aucklanduni.ac.nz>
2018-09-01 00:55:26 +1200

    Added some states for the nomination container

--- client/src/components/NominationContainer.tsx
+++ client/src/components/NominationContainer.tsx
@@ -1,18 +1,65 @@
 import * as React from "react";
 
 export interface INominationContainerProps {
-  nominationKey: string;
+  nominationID: string;
+  open: boolean;
 }
 
-class NominationContainer extends React.Component<
-  INominationContainerProps,
-  any
-  > {
-  /* public state = {
-    nominationKey: String,
-  }; */
+class NominationContainer extends React.Component<any, any> {
+  public state = {
+    nominationID: "",
+    img: "",
+    nominee: "",
+    nominator: "",
+    category: "",
+    justification: "",
+    upvoters: [] as any[],
+    comments: [] as any[]
+  };
+
+  public static defaultProps = {
+    isModal: true
+  };
+
+  public onClose = () => {
+    this.props.history.goBack();
+  };
+
+  public componentDidMount() {
+    // Get the nomination info and
+  }
+
   public render() {
-    return <h1>ddddddd</h1>;
+    return (
+      <div
+        className="modal"
+        style={{
+          display: "block",
+          position: this.props.isModal ? "fixed" : "static"
+        }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+
+              {this.props.isModal && (
+                <button
+                  className="close"
+                  onClick={this.props.onClose || this.onClose}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              )}
+            </div>
+
+            <div className="modal-body">
+              <p>Modal body</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
