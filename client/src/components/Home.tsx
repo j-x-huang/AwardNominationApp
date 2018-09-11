c60e05b8bc2411a5538863075e23046215aa3d6f
Josh Asi <josh.asi@hotmail.co.nz>
2018-09-11 14:49:04 +1200

    Changed the home screen image

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -9,8 +9,8 @@ class Home extends React.Component<any, any> {
   public render() {
     return (
       <img
-        style={{ width: "100%", filter: "blur(0.5px)" }}
-        src="https://pbs.twimg.com/media/DXX0Id4VMAEwa7a.jpg"
+        style={{ width: "100%"}}
+        src="http://hdqwalls.com/download/purple-sunset-in-ocean-2560x1440.jpg"
       />
     );
   }
