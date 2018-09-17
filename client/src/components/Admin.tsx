af7ea9de42e5cc1afc807f62d28a435a2818b53a
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2018-09-17 17:45:40 +1200

    Added initial Look and feel to Admin page
    Fixed some sizing issues on larger screen for nomination form with media queries
    patch to margins

--- client/src/components/Admin.tsx
+++ client/src/components/Admin.tsx
@@ -2,41 +2,53 @@ import * as React from "react";
 import "../App.css";
 
 class Admin extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-    }
-    public state = {
-        isLockDown: false
-    }
-    public render() {
-        return (
-            <div>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={this.lockDown}
-              >
-                Lock
-              </button>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={this.filterTally}
-              >
-                Tally
-              </button>
-            </div>
-        );
-    }
-    private lockDown = () => {
-        this.state.isLockDown = !this.state.isLockDown;
-        console.log('Lock down ' + this.state.isLockDown);
-    };
+  constructor(props: any) {
+    super(props);
+  }
+  public state = {
+    isLockDown: false
+  };
+  public render() {
+    return (
+      <div id="adminDiv">
+        <div className="adminOption">
+          <div className="float-left">
+            <span className="adminOptionTitle">Tally Nominations</span>
+            <span>Export sorted nominations of each category</span>
+          </div>
+          <button
+            type="button"
+            className="btn btn-primary float-right"
+            onClick={this.lockDown}
+          >
+            Initiate Lockdown
+          </button>
+        </div>
+        <div className="adminOption">
+          <div className="float-left">
+            <span className="adminOptionTitle">Lockdown Site</span>
+            <span>Prevent further award nominations</span>
+          </div>
 
-    private filterTally = () => {
-        console.log('I will filter and tally');
-    };
+          <button
+            type="button"
+            className="btn btn-primary float-right"
+            onClick={this.filterTally}
+          >
+            Export Results
+          </button>
+        </div>
+      </div>
+    );
+  }
+  private lockDown = () => {
+    this.state.isLockDown = !this.state.isLockDown;
+    console.log("Lock down " + this.state.isLockDown);
+  };
 
+  private filterTally = () => {
+    console.log("I will filter and tally");
+  };
 }
 
 export default Admin;
