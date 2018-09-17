b8afc8594d1ffdc287961ca0c0d5501f1c220984
Unknown <h.chuk@auckland.ac.nz>
2018-09-17 19:36:21 +1200

    Fixed tally and lockdown labels

--- client/src/components/Admin.tsx
+++ client/src/components/Admin.tsx
@@ -14,8 +14,8 @@ class Admin extends React.Component<any, any> {
       <div id="adminDiv">
         <div className="adminOption">
           <div className="float-left">
-            <span className="adminOptionTitle">Tally Nominations</span>
-            <span>Export sorted nominations of each category</span>
+          <span className="adminOptionTitle">Lockdown Site</span>
+            <span>Prevent further award nominations</span>
           </div>
           <button
             type="button"
@@ -27,10 +27,9 @@ class Admin extends React.Component<any, any> {
         </div>
         <div className="adminOption">
           <div className="float-left">
-            <span className="adminOptionTitle">Lockdown Site</span>
-            <span>Prevent further award nominations</span>
+          <span className="adminOptionTitle">Tally Nominations</span>
+            <span>Export sorted nominations of each category</span>
           </div>
-
           <button
             type="button"
             className="btn btn-primary float-right"
